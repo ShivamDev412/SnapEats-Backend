683717c1d040eebbb17f2bfda08bac23d85f43d2
Shivam Singh <shiv41297@gmail.com>
2024-05-31 06:08:16 -0400

    cors env update

--- src/utils/UploadToS3.ts
+++ src/utils/UploadToS3.ts
@@ -1,139 +1,135 @@
 import {
-    S3Client,
-    GetObjectCommand,
-    PutObjectCommand,
-    DeleteObjectCommand,
-  } from "@aws-sdk/client-s3";
-  import crypto from "crypto";
-  import dotenv from "dotenv";
-  import sharp from "sharp";
-  import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+  DeleteObjectCommand,
+} from "@aws-sdk/client-s3";
+import crypto from "crypto";
+import dotenv from "dotenv";
+import sharp from "sharp";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+dotenv.config();
 
-  dotenv.config();
-  // @ts-ignore
-  const s3 = new S3Client({
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    },
-    region: process.env.AWS_REGION,
-  });
-  export const uploadToS3 = async (
-    name: string,
-    image: any,
-    contentType: any
-  ) => {
-    const compressedImage = await sharp(image)
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+  region: process.env.AWS_REGION!,
+});
+export const uploadToS3 = async (
+  name: string,
+  image: any,
+  contentType: any
+) => {
+  const compressedImage = await sharp(image)
+    .resize({
+      width: 1920,
+      height: 1080,
+      fit: "contain",
+    })
+    .toFormat("webp")
+    .toBuffer();
+  const generateRandomName = (byteLength = 32) => {
+    const randomBytes = crypto.randomBytes(byteLength);
+    return randomBytes.toString("hex");
+  };
+  const randomImageName = generateRandomName();
+
+  const params = {
+    Bucket: process.env.S3_BUCKET_NAME!,
+    Key: randomImageName,
+    Body: compressedImage,
+    ContentType: contentType,
+  };
+
+  try {
+    const command = new PutObjectCommand(params);
+    await s3.send(command);
+    console.log("Image uploaded successfully to S3", randomImageName);
+    return randomImageName;
+  } catch (error) {
+    console.error("Error uploading image to S3:", error);
+    throw error;
+  }
+};
+export const getImage = async (name: string) => {
+  const getObjParam = {
+    Bucket: process.env.S3_BUCKET_NAME!,
+    Key: name,
+  };
+  const command = new GetObjectCommand(getObjParam);
+  const url = await getSignedUrl(s3, command, { expiresIn: 60 * 60 * 24 * 7 });
+  if (url) {
+    return url;
+  }
+};
+
+export const uploadCompressedImageToS3 = async (
+  name: string,
+  image: any,
+  contentType: any
+) => {
+  const targetFileSize = 50 * 1024;
+
+  let compressedImageBuffer = await sharp(image)
+    .resize({
+      width: 89,
+      height: 51,
+      fit: "fill",
+    })
+    .toFormat("webp")
+    .toBuffer({ resolveWithObject: true });
+
+  let quality = 90;
+  while (compressedImageBuffer.info.size > targetFileSize && quality >= 10) {
+    compressedImageBuffer = await sharp(image)
       .resize({
-        width: 1920,
-        height: 1080,
+        width: 4,
+        height: 4,
         fit: "contain",
       })
-      .toFormat("webp")
-      .toBuffer();
-    const generateRandomName = (byteLength = 32) => {
-      const randomBytes = crypto.randomBytes(byteLength);
-      return randomBytes.toString("hex");
-    };
-    const randomImageName = generateRandomName();
-  
-    const params = {
-      Bucket: process.env.S3_BUCKET_NAME!,
-      Key: randomImageName,
-      Body: compressedImage,
-      ContentType: contentType,
-    };
-  
-    try {
-      const command = new PutObjectCommand(params);
-      await s3.send(command);
-      console.log("Image uploaded successfully to S3", randomImageName);
-      return randomImageName;
-    } catch (error) {
-      console.error("Error uploading image to S3:", error);
-      throw error;
-    }
-  };
-  export const getImage = async (name: string) => {
-    const getObjParam = {
-      Bucket: process.env.S3_BUCKET_NAME!,
-      Key: name,
-    };
-    const command = new GetObjectCommand(getObjParam);
-    const url = await getSignedUrl(s3, command, { expiresIn: 60 * 60 * 24 * 7 });
-    if (url) {
-      return url;
-    }
-  };
-  
-  export const uploadCompressedImageToS3 = async (
-    name: string,
-    image: any,
-    contentType: any
-  ) => {
-    const targetFileSize = 50 * 1024; 
-  
-    let compressedImageBuffer = await sharp(image)
-      .resize({
-        width: 89, 
-        height: 51,
-        fit: "fill",
-      })
-      .toFormat('webp')
+      .webp({ quality })
       .toBuffer({ resolveWithObject: true });
-  
-    let quality = 90;
-    while (compressedImageBuffer.info.size > targetFileSize && quality >= 10) {
-      compressedImageBuffer = await sharp(image)
-        .resize({
-          width: 4, 
-          height: 4,
-          fit: 'contain',
-        })
-        .webp({ quality })
-        .toBuffer({ resolveWithObject: true });
-      quality -= 10;
-    }
-  
-    console.log(
-      `Final image size: ${compressedImageBuffer.info.size} bytes with quality ${quality}`
-    );
-  
-    const generateRandomName = (byteLength = 32) => {
-      const randomBytes = crypto.randomBytes(byteLength);
-      return randomBytes.toString('hex');
-    };
-    const randomImageName = generateRandomName();
-  
-    const params = {
-      Bucket: process.env.S3_BUCKET_NAME!,
-      Key: randomImageName,
-      Body: compressedImageBuffer.data,
-      ContentType: contentType,
-    };
-  
-    try {
-      const command = new PutObjectCommand(params);
-      await s3.send(command);
-      return randomImageName;
-    } catch (error) {
-      console.error('Error uploading image to S3:', error);
-      throw error;
-    }
-  };
-  export const deleteImageFromS3 = async (name: string) => {
-    const params = {
-      Bucket: process.env.S3_BUCKET_NAME!,
-      Key: name,
-    };
-    try {
-      await s3.send(new DeleteObjectCommand(params));
-      console.log(`Image ${name} deleted from S3`);
-    } catch (error) {
-      console.error('Error deleting image from S3:', error);
-      throw error;
-    }
+    quality -= 10;
   }
-  
-  
-  
+
+  console.log(
+    `Final image size: ${compressedImageBuffer.info.size} bytes with quality ${quality}`
+  );
+
+  const generateRandomName = (byteLength = 32) => {
+    const randomBytes = crypto.randomBytes(byteLength);
+    return randomBytes.toString("hex");
+  };
+  const randomImageName = generateRandomName();
+
+  const params = {
+    Bucket: process.env.S3_BUCKET_NAME!,
+    Key: randomImageName,
+    Body: compressedImageBuffer.data,
+    ContentType: contentType,
+  };
+
+  try {
+    const command = new PutObjectCommand(params);
+    await s3.send(command);
+    return randomImageName;
+  } catch (error) {
+    console.error("Error uploading image to S3:", error);
+    throw error;
+  }
+};
+export const deleteImageFromS3 = async (name: string) => {
+  const params = {
+    Bucket: process.env.S3_BUCKET_NAME!,
+    Key: name,
+  };
+  try {
+    await s3.send(new DeleteObjectCommand(params));
+    console.log(`Image ${name} deleted from S3`);
+  } catch (error) {
+    console.error("Error deleting image from S3:", error);
+    throw error;
+  }
+};
